43c1b3660b5ac387bf0812bb7f523554d5251da6
Sumit Tomar <tomarsumit570@gmail.com>
2022-07-01 21:17:06 +0530

    Patterns

---
+++ 02. Patterns/pattern1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class pattern1 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        
+    }
+}
